044bc2899781a48dc3009ef22b359f47029e6648
Xiaojia <gracegreat1killer@gmail.com>
2015-12-22 18:40:56 +0800

    Update login.jsx

--- src/app/components/login.jsx
+++ src/app/components/login.jsx
@@ -1,14 +1,47 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, Link } from 'react-router'
+import { AppCanvas, AppBar, Avatar, FlatButton, LeftNav, List, ListItem, Mixins, Styles, TextField, RaisedButton } from 'material-ui'
 
 
 class Login extends React.Component {
+
+	submit(e) {
+		let username = $("#username").val().trim();
+		let password = $("#password").val().trim();
+		
+	    if (!username || !password) {
+	      return false;
+	    }
+
+		$.post("/#/index.html", {username: username, password: password}, function(){
+			window.localtion = "/index.html";
+		})
+		.fail(function(){
+			alert("error!");
+		})	
+		return false;
+	}
+
     render() {
         return(
-            <h1>Fill in this class to implement Login Page</h1>
+        	<AppCanvas>
+        		<form onSubmit={this.submit}>
+	                <TextField id="username"
+	                 hintText="Username Field"
+	                 floatingLabelText="username"/>
+	      
+	                <TextField id="password"
+					 hintText="Password Field"
+					 floatingLabelText="password"
+					 type="password" />
+					<RaisedButton label="Login" type="submit"/>
+		        </form>
+        	</AppCanvas>
         )
     }
+
 }
 
 export default Login
+
