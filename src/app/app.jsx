3ccee59acbd581a2ac7368d55b347c255f5809cc
brickgao <brickgao@gmail.com>
2015-12-22 20:12:02 +0800

    add subtitle

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { browserHistory, Router, Route, Link } from 'react-router'
+import { browserHistory, IndexRoute, Router, Route, Link } from 'react-router'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 import Master from './components/master'
 import Login from './components/login'
