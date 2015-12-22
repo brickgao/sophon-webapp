fe8601295fd660534e66242e1439b1429f7bb705
brickgao <brickgao@gmail.com>
2015-12-22 20:11:39 +0800

    Change status to fonticon

--- src/app/components/server_status.jsx
+++ src/app/components/server_status.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, Link } from 'react-router'
 import {
+    FontIcon,
+    Styles,
     Table,
     TableBody,
     TableFooter,
@@ -11,6 +13,8 @@ import {
     TableRowColumn,
     RaisedButton,
 } from 'material-ui'
+
+const { Colors } = Styles
 
 
 class ServerStatus extends React.Component {
@@ -33,9 +37,9 @@ class ServerStatus extends React.Component {
              displaySelectAll={false}>
                 <TableRow>
                     <TableHeaderColumn>#</TableHeaderColumn>
+                    <TableHeaderColumn>Status</TableHeaderColumn>
                     <TableHeaderColumn>Hostname</TableHeaderColumn>
                     <TableHeaderColumn>IP Address</TableHeaderColumn>
-                    <TableHeaderColumn>Status</TableHeaderColumn>
                     <TableHeaderColumn>CPU Loads</TableHeaderColumn>
                     <TableHeaderColumn>Memory Usage</TableHeaderColumn>
                     <TableHeaderColumn>Disk Usage</TableHeaderColumn>
@@ -57,29 +61,29 @@ class ServerStatus extends React.Component {
              displayRowCheckbox={false}>
                 <TableRow>
                     <TableRowColumn>1</TableRowColumn>
+                    <TableRowColumn><FontIcon className="material-icons" color={Colors.greenA400}>done</FontIcon></TableRowColumn>
                     <TableRowColumn>Marvin</TableRowColumn>
                     <TableRowColumn>123.456.78.1</TableRowColumn>
-                    <TableRowColumn>Active</TableRowColumn>
                     <TableRowColumn>0.2</TableRowColumn>
-                    <TableRowColumn>2000000 / 8030376 MB</TableRowColumn>
+                    <TableRowColumn>2000 / 8030 MB</TableRowColumn>
                     <TableRowColumn>20 / 80 GB</TableRowColumn>
                     <TableRowColumn><RaisedButton label="Detail" secondary={true} /></TableRowColumn>
                 </TableRow>
                 <TableRow>
                     <TableRowColumn>2</TableRowColumn>
+                    <TableRowColumn><FontIcon className="material-icons" color={Colors.greenA400}>done</FontIcon></TableRowColumn>
                     <TableRowColumn>Orion</TableRowColumn>
                     <TableRowColumn>123.456.78.2</TableRowColumn>
-                    <TableRowColumn>Active</TableRowColumn>
                     <TableRowColumn>0.1</TableRowColumn>
-                    <TableRowColumn>3000000 / 8030376 MB</TableRowColumn>
+                    <TableRowColumn>3000 / 8030 MB</TableRowColumn>
                     <TableRowColumn>15 / 80 GB</TableRowColumn>
                     <TableRowColumn><RaisedButton label="Detail" secondary={true} /></TableRowColumn>
                 </TableRow>
                  <TableRow>
                     <TableRowColumn>3</TableRowColumn>
+                    <TableRowColumn><FontIcon className="material-icons" color={Colors.red400}>error</FontIcon></TableRowColumn>
                     <TableRowColumn>Alice</TableRowColumn>
                     <TableRowColumn>123.456.78.4</TableRowColumn>
-                    <TableRowColumn>Down</TableRowColumn>
                     <TableRowColumn>0</TableRowColumn>
                     <TableRowColumn>0 / 0 MB</TableRowColumn>
                     <TableRowColumn>0 / 0 GB</TableRowColumn>
@@ -87,11 +91,11 @@ class ServerStatus extends React.Component {
                 </TableRow>
                 <TableRow>
                     <TableRowColumn>4</TableRowColumn>
+                    <TableRowColumn><FontIcon className="material-icons" color={Colors.greenA400}>done</FontIcon></TableRowColumn>
                     <TableRowColumn>Bob</TableRowColumn>
                     <TableRowColumn>123.456.78.5</TableRowColumn>
-                    <TableRowColumn>Active</TableRowColumn>
                     <TableRowColumn>0.8</TableRowColumn>
-                    <TableRowColumn>70000000 / 8030376 MB</TableRowColumn>
+                    <TableRowColumn>7000 / 8030 MB</TableRowColumn>
                     <TableRowColumn>40 / 80 GB</TableRowColumn>
                     <TableRowColumn><RaisedButton label="Detail" secondary={true} /></TableRowColumn>
                 </TableRow>
