5f3af855958c23b7068360eaee50c511a87cdc5c
brickgao <brickgao@gmail.com>
2015-12-23 00:21:05 +0800

    redesign login page

--- src/app/components/login.jsx
+++ src/app/components/login.jsx
@@ -1,46 +1,159 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, Link } from 'react-router'
-import { AppCanvas, AppBar, Avatar, FlatButton, LeftNav, List, ListItem, Mixins, Styles, TextField, RaisedButton } from 'material-ui'
+import {
+    AppCanvas,  
+    Paper,
+    Styles,
+    RaisedButton,
+    TextField,
+} from 'material-ui'
+
+const { Colors } = Styles
 
 
 class Login extends React.Component {
 
-	submit(e) {
-		let username = $("#username").val().trim();
-		let password = $("#password").val().trim();
-		
-	    if (!username || !password) {
-	      return false;
-	    }
+    constructor(props) {
+        super(props)
+        this.state = {
+            passwordErrorText: "",
+            usernameErrorText: "",
+        }
+        this._submit = this._submit.bind(this)
+        this._handleData = this._handleData.bind(this)
+    }
 
-		$.post("/#/index.html", {username: username, password: password}, function(){
-			window.localtion = "/index.html";
-		})
-		.fail(function(){
-			alert("error!");
-		})	
-		return false;
-	}
+    _submit() {
+        let username = $("#username").val()
+        let password = $("#password").val()
+
+        try {
+            this.setState({
+                usernameErrorText: "",
+                passwordErrorText: "",
+            })
+        }
+        finally {
+            try {
+                if (!username.trim()) {
+                    this.setState({usernameErrorText: "Please input username"})
+                }
+                if (!password.trim()) {
+                    this.setState({passwordErrorText: "Please input password"})
+                }
+            }
+            finally {
+                $.post(
+                    "/api/user/login",
+                    {username: username, password: password},
+                    this._handleData,
+                )
+            }
+        }
+    }
+
+    _handleData(data) {
+        if (data["id"] != null) {
+            window.location = "/#/"
+        }
+        else {
+            this.setState({passwordErrorText: "Wrong username or password"})
+        }
+    }
 
     render() {
+        let styles = {
+            position: 'fixed',
+            width: '100%',
+            height: '100%',
+            backgroundColor: Colors.grey200,
+        }
+
         return(
         	<AppCanvas>
-        		<form onSubmit={this.submit}>
-	                <TextField id="username"
-	                 hintText="Username Field"
-	                 floatingLabelText="username"/>
-	      
-	                <TextField id="password"
-					 hintText="Password Field"
-					 floatingLabelText="password"
-					 type="password" />
-					<RaisedButton label="Login" type="submit"/>
-		        </form>
+                <div style={styles}>
+                    {this._getLoginPaper()}
+                </div>
         	</AppCanvas>
         )
     }
+    
+    _getLoginPaper() {
+        let styles = {
+            width: '328px',
+            height: '380px',
+            position: 'fixed',
+            top: '50%',
+            left: '50%',
+            marginTop: '-164px',
+            marginLeft: '-190px',
+        }
 
+        return (
+            <Paper zDepth={2} style={styles}>
+                {this._getForm()}
+            </Paper>
+        )
+    }
+
+    _getForm() {
+        let styles = {
+            marginTop: '20%',
+            marginLeft: '35px',
+        }
+
+        return (
+            <div style={styles}>
+                {this._getFormHeader()}
+                {this._getFormTextField()}
+                {this._getFormBtn()}
+            </div>
+        )
+    }
+
+    _getFormHeader() {
+        let styles = {
+            fontSize: '50px',
+            fontWeight: '300',
+            color: Colors.lightBlue300,
+            marginBottom: '20px',
+        }
+
+        return (
+            <h2 style={styles}>Login</h2>
+        )
+    }
+
+    _getFormTextField() {
+        return (
+            <div>
+                <TextField
+                 id="username"
+                 errorText={this.state.usernameErrorText}
+                 floatingLabelText="Username"
+                 hintText="Input your username" />
+                <TextField
+                 id="password"
+                 errorText={this.state.passwordErrorText}
+                 floatingLabelText="Password"
+                 type="password"
+                 hintText="Input your Password" />
+            </div>
+        )
+    }
+
+    _getFormBtn() {
+        let styles = {
+            position: 'absolute',
+            left: '203px',
+            bottom: '20px',
+        }
+
+        return (
+            <RaisedButton label="Submit" secondary={true} style={styles} onTouchTap={this._submit}/>
+        )
+    }
 }
 
 export default Login
