83c4a727c6b530c0043bce4848a6b8072e5ba4da
brickgao <brickgao@gmail.com>
2016-02-19 21:47:55 +0800

    finish the part that could determine whether the user is logined or not

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -17,20 +17,34 @@ import DeployDetail from './components/deploy_detail'
 injectTapEventPlugin()
 
 
+function requireAuth(nextState, replaceState) {
+    $.ajax({
+        type: "GET",
+        url: "/api/user/info",
+        async: false,
+        success: function (data) {
+            if (!data.hasOwnProperty("id")) {
+                replaceState({}, "/login")
+            }
+        },
+    })
+}
+
+
 render((
   <Router history={browserHistory}>
     <Route path="/" component={Master}>
         <Route path="login" component={Login} />
-        <Route path="server" component={ServerStatus}>
+        <Route path="server" component={ServerStatus} onEnter={requireAuth}>
             <IndexRoute component={ServerStatusIndex} />
-            <Route path=":id" component={ServerStatusDetail} />
+            <Route path=":id" component={ServerStatusDetail}/>
         </Route>
-        <Route path="deploy" component={Deploy}>
+        <Route path="deploy" component={Deploy} onEnter={requireAuth}>
             <IndexRoute component={DeployIndex} />
             <Route path=":id" component={DeployDetail} />
         </Route>
-        <Route path="ssh_permission" component={SSHPermission} />
-        <Route path="dockers_status" component={DockersStatus} />
+        <Route path="ssh_permission" component={SSHPermission} onEnter={requireAuth}/>
+        <Route path="dockers_status" component={DockersStatus} onEnter={requireAuth}/>
     </Route>
   </Router>
 ), document.getElementById('app'))
